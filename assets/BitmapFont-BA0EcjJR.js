460e464c8708003427417fb7405dab06d96e4b59
Rayden Zhang <rayden3651@outlook.com>
2026-05-17 22:22:35 -0400

    Updates

--- assets/BitmapFont-BA0EcjJR.js
+++ assets/BitmapFont-BA0EcjJR.js
@@ -1,4 +1,4 @@
-import{D as st,T as q,h as Y,M as dt,l as ot,F as tt,at,E as Wt,aq as Et,r as V,ao as ut,ap as ft,a5 as N,as as $,L as pt,R as nt,I as vt,f as O,ag as Pt}from"./index-BEm7oQXV.js";import{C as gt}from"./CanvasPool-DgBFhyJ_.js";/**
+import{D as st,T as q,h as Y,M as dt,l as ot,F as tt,at,E as Wt,aq as Et,r as V,ao as ut,ap as ft,a5 as N,as as $,L as pt,R as nt,I as vt,f as O,ag as Pt}from"./index-CCoTENCJ.js";import{C as gt}from"./CanvasPool-C8h5-Z-A.js";/**
  * tiny-lru
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
--- assets/BufferResource-I62_rpJu.js
+++ assets/BufferResource-I62_rpJu.js
@@ -1,4 +1,4 @@
-import{at as U,ab as g,S as c,ar as S,d as _,e as m,E as I,aq as p}from"./index-BEm7oQXV.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{at as U,ab as g,S as c,ar as S,d as _,e as m,E as I,aq as p}from"./index-CCoTENCJ.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
