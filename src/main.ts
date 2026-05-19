8d6148ad3a710cda1f9ed3e1aa691549e31248d8
Rayden Zhang <rayden3651@outlook.com>
2026-05-18 22:05:58 -0400

    Added mutations and a little something

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import {
+  Application,
+  Assets,
+  Container,
+  Graphics,
+  Sprite,
+  Text,
+  type FederatedPointerEvent,
+} from "pixi.js";
 import Swal from "sweetalert2";
 import { clear, get, set } from "idb-keyval";
 
@@ -10,9 +18,17 @@ interface Egg {
   src: string;
 }
 
+interface Mutation {
+  id: string;
+  name: string;
+  chance: number;
+  type: "positive" | "neutral" | "negative" | "variable";
+  effect: number | ((egg: Egg) => number | Promise<number>);
+}
+
 interface Instance {
   id: string;
-  uuid: ReturnType<typeof crypto.randomUUID>;
+  mutation: string;
   x: number;
   y: number;
 }
@@ -33,6 +49,7 @@ interface SettingOption {
 
 interface Config {
   eggs: Egg[];
+  mutations: Mutation[];
 }
 
 (async () => {
@@ -76,6 +93,30 @@ interface Config {
         src: `eggs/rainbow.png`,
       },
     ].map((e) => ({ ...e, src: `${base}${e.src}` })),
+
+    mutations: [
+      {
+        id: "none",
+        name: "None",
+        chance: 80,
+        type: "neutral",
+        effect: 1.0,
+      },
+      {
+        id: "big",
+        name: "Big",
+        chance: 30,
+        type: "positive",
+        effect: 1.2,
+      },
+      {
+        id: "double-yolk",
+        name: "Double Yolk",
+        chance: 5,
+        type: "positive",
+        effect: 2, // Double the yolk, so double the price!
+      },
+    ],
   };
 
   const app = new Application();
@@ -90,6 +131,95 @@ interface Config {
   });
 
   document.body.appendChild(app.canvas);
+
+  app.stage.sortableChildren = true;
+
+  // Mutation tooltip
+  const mutationTooltip = {
+    container: new Container(),
+    text: new Text({
+      text: "",
+      style: {
+        fontFamily: "Verdana",
+        fill: 0xffffff,
+        fontSize: 14,
+      },
+    }),
+    bg: new Graphics(),
+    offset: {
+      x: 15,
+      y: 15,
+      padding: 8,
+    },
+    init() {
+      this.container.visible = false;
+      this.container.zIndex = 1000;
+      this.container.addChild(this.bg, this.text);
+
+      app.stage.addChild(this.container);
+    },
+
+    /** Use in the `pointerover` event */
+    onPointerOver(
+      event: FederatedPointerEvent,
+      mutation: Mutation,
+      price: number,
+    ) {
+      this.text.text = `${mutation.name} ($${price})`;
+      this.text.x = this.offset.padding;
+      this.text.y = this.offset.padding;
+
+      const color = (() => {
+        switch (mutation.type) {
+          case "neutral": {
+            return "white";
+          }
+
+          case "positive": {
+            return "lawngreen";
+          }
+
+          case "negative": {
+            return "red";
+          }
+
+          case "variable": {
+            return "yellow";
+          }
+        }
+      })();
+      this.text.style.fill = color;
+
+      const width = this.text.width + this.offset.padding * 2;
+      const height = this.text.height + this.offset.padding * 2;
+
+      this.bg.clear();
+      this.bg.fill({ color: 0x515157 });
+      this.bg.roundRect(0, 0, width, height, 4);
+      this.bg.stroke({ color, width: 4 });
+      this.bg.fill();
+
+      this.container.x = event.global.x + this.offset.x;
+      this.container.y = event.global.y + this.offset.y;
+
+      this.container.visible = true;
+    },
+
+    /** Use in the `pointermove` event */
+    onPointerMove(event: FederatedPointerEvent) {
+      if (this.container.visible) {
+        this.container.x = event.global.x + this.offset.x;
+        this.container.y = event.global.y + this.offset.y;
+      }
+    },
+
+    /** Use in the `pointerout` event, and also call this right before destroying the egg sprite */
+    onPointerOut() {
+      this.container.visible = false;
+    },
+  };
+
+  mutationTooltip.init();
 
   const selectEgg = () => {
     const totalWeight = config.eggs.reduce((acc, egg) => acc + egg.chance, 0);
@@ -109,20 +239,39 @@ interface Config {
     return config.eggs[config.eggs.length - 1];
   };
 
+  const selectMutation = () => {
+    const totalWeight = config.mutations.reduce(
+      (acc, egg) => acc + egg.chance,
+      0,
+    );
+    if (totalWeight === 0) return config.mutations[0];
+
+    const randomRoll = Math.random() * totalWeight;
+    let cursor = 0;
+
+    for (const mutation of config.mutations) {
+      cursor += mutation.chance;
+      if (randomRoll < cursor) {
+        return mutation;
+      }
+    }
+
+    // Fallback for extreme floating point errors
+    return config.mutations[config.mutations.length - 1];
+  };
+
   interface Stats {
     eggs: Record<string, number> & { total: number };
     money: number;
   }
 
-  const stats: Stats = (await get("stats"))
-    ? ((await get("stats")) as Stats)
-    : {
-        eggs: {
-          total: 0,
-          ...Object.fromEntries(config.eggs.map((e) => [e.id, 0])),
-        },
-        money: 0,
-      };
+  const stats: Stats = {
+    eggs: {
+      total: 0,
+      ...Object.fromEntries(config.eggs.map((e) => [e.id, 0])),
+    },
+    money: 0,
+  };
 
   const instances: Set<Instance> = (await get("instances"))
     ? ((await get("instances")) as Set<Instance>)
@@ -162,10 +311,21 @@ interface Config {
 
   updateTotal(); // Initialize the counters
 
-  const makeEgg = async (e: KeyboardEvent | MouseEvent) => {
+  const makeEgg = async (
+    e: KeyboardEvent | MouseEvent,
+    precreatedInstance?: Instance,
+  ) => {
     if (e.type === "click" || ("code" in e && e.code === "KeyE")) {
       // Generate random positions
-      const egg = selectEgg();
+      const egg =
+        config.eggs.find((e) => e.id === precreatedInstance?.id) || selectEgg();
+      const mutation =
+        config.mutations.find((e) => e.id === precreatedInstance?.mutation) ||
+        selectMutation();
+      const price =
+        typeof mutation.effect === "number"
+          ? egg.price * mutation.effect
+          : egg.price * (await mutation.effect(egg));
       const texture = await Assets.load(egg.src);
       const sprite = new Sprite(texture);
 
@@ -177,26 +337,39 @@ interface Config {
       const minY = sprite.height / 2;
       const maxY = app.screen.height - sprite.height / 2;
 
-      sprite.x = Math.random() * (maxX - minX) + minX;
-      sprite.y = Math.random() * (maxY - minY) + minY;
+      sprite.x = precreatedInstance?.x || Math.random() * (maxX - minX) + minX;
+      sprite.y = precreatedInstance?.y || Math.random() * (maxY - minY) + minY;
 
-      const instance = {
+      const instance = precreatedInstance || {
         id: egg.id,
-        uuid: crypto.randomUUID(),
         x: sprite.x,
         y: sprite.y,
+        mutation: mutation.id,
       };
 
       instances.add(instance);
 
       sprite.eventMode = "static";
-      sprite.on("click", () => {
+      sprite.on("pointertap", () => {
         sprite.destroy();
         stats.eggs.total--;
         stats.eggs[egg.id]--;
-        stats.money += egg.price;
+        stats.money += price;
         instances.delete(instance);
+        mutationTooltip.onPointerOut();
         updateTotal();
+      });
+
+      sprite.on("pointerover", (e) => {
+        mutationTooltip.onPointerOver(e, mutation, price);
+      });
+
+      sprite.on("pointermove", (e) => {
+        mutationTooltip.onPointerMove(e);
+      });
+
+      sprite.on("pointerout", () => {
+        mutationTooltip.onPointerOut();
       });
 
       stats.eggs.total++;
@@ -216,31 +389,10 @@ interface Config {
   // Load saved instances
   if (instances.size > 0) {
     for (const instance of instances) {
-      const egg = config.eggs.find((e) => e.id === instance.id);
-
-      if (!egg) continue;
-
-      const texture = await Assets.load(egg.src);
-      const sprite = new Sprite(texture);
-
-      sprite.anchor.set(0.5);
-
-      sprite.x = instance.x;
-      sprite.y = instance.y;
-
-      sprite.eventMode = "static";
-      sprite.on("click", () => {
-        sprite.destroy();
-        stats.eggs.total--;
-        stats.eggs[egg.id]--;
-        stats.money += egg.price;
-        instances.delete(instance);
-        updateTotal();
-      });
-
-      app.stage.addChild(sprite);
+      await makeEgg(new MouseEvent("click"), instance);
     }
   }
+
   // Game saving
   const saveGame = async () => {
     const el = document.querySelector(`#save-msg`) as HTMLDivElement;
@@ -412,5 +564,35 @@ The game will save and restart once you change this.`,
     btn.addEventListener("click", () => {
       menu.showModal();
     });
+
+    const cheatMode = () => {
+      return {
+        makeEgg,
+        settings,
+        updateTotal,
+        config,
+        selectEgg,
+        selectMutation,
+        saveGame,
+        wipeSave,
+      };
+    };
+
+    (window as any).Gulcheat = cheatMode;
+    (window as any).cheatGul = async () => {
+      alert(`Guess you didn't realize the 'Gul' stood for gullible!`);
+      alert(
+        `Also go fuck yourself, but preserve some of your dignity by playing this game:\nhttps://jjohnstongames.itch.io/the-roottrees-are-dead`,
+      );
+      await saveGame();
+      window.open("", "_self")?.close();
+    };
+
+    console.log(
+      `[=== Ready to cheat in some eggs or just checking around? ===]`,
+    );
+    console.log(
+      `[=== psst if youre choosing the first option type in window.cheatGul() and press Enter ===]`,
+    );
   }
 })();
